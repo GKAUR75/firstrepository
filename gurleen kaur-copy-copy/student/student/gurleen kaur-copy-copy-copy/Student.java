34041508aa9b4c6d10d438809275627b1d50f694
GKAUR75 <g.kaur.75@student.scu.edu.au>
2024-07-06 17:59:57 +1000

    adding variables
    adding variables

---
+++ gurleen kaur-copy-copy/student/student/gurleen kaur-copy-copy-copy/Student.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student
+{
+    private String name;
+    private int total_mark;
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public Student() {
+        
+    }
+}
